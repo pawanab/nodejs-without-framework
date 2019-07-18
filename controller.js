a7d0bed5fda92625b9be2931ed2eb6ff6774b389
Pawan Arora <pawan.arora@voicetree.co>
2019-07-18 14:57:32 +0530

    post route and 404 route implemented

--- controller.js
+++ controller.js
@@ -6,11 +6,23 @@ module.exports = http.createServer((req, res) => {
     var service = require('./service.js');
     const reqUrl = url.parse(req.url, true);
 
-    if ((reqUrl.pathname == '/sample' || reqUrl.pathname == '/' ) && req.method === 'GET') {
+    if (('/sample' == reqUrl.pathname || '/' == reqUrl.pathname ) && 'GET' === req.method) {
         console.log('Request Type : ' +
             req.method + ' EndPoint : ' +
             reqUrl.pathname);
 
             service.sampleRequest(req, res);
+    } else if ('/send-msg' == reqUrl.pathname && 'POST' === req.method ) {
+        console.log('Request Type : ' +
+            req.method + ' EndPoint : ' +
+            reqUrl.pathname);
+        
+            service.sendMsgRequest(req, res);
+    } else {
+        console.log('Request Type:' +
+            req.method + ' Invalid Endpoint: ' +
+            reqUrl.pathname);
+
+        service.invalidRequest(req, res);
     }
 });
--- service.js
+++ service.js
@@ -14,3 +14,30 @@ exports.sampleRequest = function (req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.end(JSON.stringify(response));
 };
+
+exports.sendMsgRequest = function (req, res) {
+  body = '';
+  // to get the post body from request
+  //it comes in form of stream
+  req.on('data', function (chunk) {
+      body += chunk;
+  });
+
+  // this function will execute only after stream is completed and full post body is received.
+  req.on('end', function(){
+      postBody = JSON.parse(body);
+      var response = {
+          "text" : "Name is " + postBody.value + " from " +  postBody.city
+      };
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(response));
+  });
+};
+
+exports.invalidRequest = function (req, res) {
+  res.statusCode = 404;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Invalid Request'); 
+};
